09ec8603b4f4248d092d12bee0431c9edf06cc1f
Jakub St <jakubst@MacBookAir.lan>
2025-12-15 02:52:41 +0100

    Fix lint error: remove unused info param

--- src/auth/optional-jwt-auth.guard.ts
+++ src/auth/optional-jwt-auth.guard.ts
@@ -3,7 +3,7 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class OptionalJwtAuthGuard extends AuthGuard('jwt') {
-    handleRequest(err, user, info) {
+    handleRequest(err, user) {
         // If error or no user, just return null (no authenticated user)
         // instead of throwing an exception
         if (err || !user) {
